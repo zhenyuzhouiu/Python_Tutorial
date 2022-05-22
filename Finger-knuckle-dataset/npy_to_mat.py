d0d4f5fa8c2299ea58254804ad28bc08137fd75e
zhenyuzhouiu <zhenyuzhouiu@gmail.com>
2022-05-22 18:04:19 +0800

    Add npy_to_mat and resize128to100or80 file

--- Finger-knuckle-dataset/npy_to_mat.py
+++ Finger-knuckle-dataset/npy_to_mat.py
@@ -1,13 +1,13 @@
 import numpy as np
 import scipy.io
 
-path = ["/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/3d1s3d/3d1s3d_RFN-128-shiftedloss-lr0.001-protocol.npy",
-        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/3d1s3d/3d1s3d_RFN-128-wholeimagerotationandtranslation-lr0.001-protocol.npy",
-        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/3d1s-2d/3d1s-rfn-ws-protocol.npy",
-        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/3d1s-2d/3d1s-rfn-wrs-protocol.npy",
+path = ["/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/two-fkv3/fknet.npy",
+        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/two-fkv3/WRS-0.001-protocol3.npy",
+        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/two-fkv3/WS-0.01-protocol3.npy",
+        "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/two-fkv3/WS-0.001-protocol3.npy",
         "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/codekevin/fknet/test/fkv3/fkv3-rfn-ws-protocol.npy"]
 
 
-for i in range(2):
+for i in range(1):
     data = np.load(path[i], allow_pickle=True)[()]
     scipy.io.savemat(path[i][:path[i].find('.npy')] + ".mat", {'D_genuine':data['g_scores'], 'D_imposter':data['i_scores']})
--- Finger-knuckle-dataset/resize128to100or80.py
+++ Finger-knuckle-dataset/resize128to100or80.py
@@ -1,22 +1,24 @@
 import os
 import cv2
 
-data_path = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/3DFingerKnuckle/finger/middlefinger/session1/191-228/"
+data_path = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/PolyUHD/all/"
 
-dst_path_test = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/3DFingerKnuckle/finger/caffe/middlefinger/session1/191_228_100_70/"
-dst_path_train = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/3DFingerKnuckle/finger/caffe/middlefinger/session1/191_228_80_48/"
+dst_path_test = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/PolyUHD/caffe/100-70/"
+dst_path_train = "/home/zhenyuzhou/Desktop/finger-knuckle/deep-learning/Finger-Knuckle-Recognition/dataset/PolyUHD/caffe/80-48/"
 
 subject_path = os.listdir(data_path)
 
+subject_number = 1
 for s in subject_path:
     image_path = os.listdir(os.path.join(data_path, s))
+    image_number = 1
     for i in image_path:
         image_file = os.path.join(data_path, s, i)
         image = cv2.imread(image_file)
         # change image 128x128 to 100x70
         # step1:-> rotation 90 degree
-        # image_90 = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        image_90 = image
+        image_90 = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+        # image_90 = image
         # step2:-> keep ration 128x128->128x90
         h, w, _ = image_90.shape
         center_w = int(w/2)
@@ -24,19 +26,21 @@ for s in subject_path:
         keep_ration_h = int((w * 70) / 100)
         crop_image = image_90[int((h-keep_ration_h)/2):int((h-keep_ration_h)/2)+keep_ration_h, 0:w]
         image_100_70 = cv2.resize(crop_image, (100, 70))
-        save_path = os.path.join(dst_path_test, s)
+        save_path = os.path.join(dst_path_test, str(subject_number))
         if not os.path.exists(save_path):
             os.mkdir(save_path)
-        save_file = os.path.join(save_path, i)
+        save_file = os.path.join(save_path, str(image_number) + '.' + i.split('.')[-1])
         cv2.imwrite(save_file, image_100_70)
 
         # step3:-> generate the 80x48 training image
         image_80_64 = image_100_70[10:58, 9:89]
-        save_path = os.path.join(dst_path_train, s)
+        save_path = os.path.join(dst_path_train, str(subject_number))
         if not os.path.exists(save_path):
             os.mkdir(save_path)
-        save_file = os.path.join(save_path, i)
+        save_file = os.path.join(save_path, str(image_number) + '.' + i.split('.')[-1])
         cv2.imwrite(save_file, image_80_64)
+        image_number += 1
+    subject_number += 1
 
 
 
